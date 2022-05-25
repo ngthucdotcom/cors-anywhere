7a3eae023804f5f8bdded41b18019a887fe00c26
thucln <thucln@vng.com.vn>
2022-05-25 14:06:05 +0700

    Update code

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
+const CORS_PROXY = require('cors-anywhere');
 
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
+CORS_PROXY.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+}).listen(PORT, HOST, function() {
+    console.log('Running CORS Anywhere on ' + HOST + ':' + PORT);
 });
